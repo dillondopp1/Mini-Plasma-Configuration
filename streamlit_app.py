7c95cb6bae737fa980e6224d75e08af7db231453
Dillon Dopp <dillondopp@gmail.com>
2025-12-08 12:58:21 -0700

    Update default profit per sqft to 40

--- streamlit_app.py
+++ streamlit_app.py
@@ -499,7 +499,7 @@ def main():
     profit_per_sqft = st.sidebar.number_input(
         "Profit per square foot of nominal area ($/ft²)",
         min_value=0.0,
-        value=45.0,
+        value=40.0,
         step=5.0,
         help="Additional profit for each square foot of nominal working area.",
     )
